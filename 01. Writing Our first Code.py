91c0bad8b523127163f64c88e2a933e5629f4973
tassamoo <tassamu@outlook.com>
2021-03-24 22:05:14 +0700

    adding learning python files

---
+++ 01. Writing Our first Code.py
@@ -0,0 +1,21 @@
+print("Hello World") # This line print Hello World
+print(50) #tis Line prints 50
+print(1000)
+print(3.142)
+print(50, 1000, 3.142, "Hello World")
+
+print("Hello", end="")
+print("World")
+
+print("Hello", end=" ")
+print("World")
+
+# This is Just a comment hanging on its own!
+# For multi-line comments, we must
+# add the hashtag symbol
+# each line
+
+""" Docstrings are pretty cool
+for writing longer comments
+or notes about the code
+"""
